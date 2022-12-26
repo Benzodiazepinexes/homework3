351d057e975838323132825a5e1c9419cacd55c7
Денис Михальчук <denismykhalchukofficial@gmail.com>
2022-12-26 16:04:49 +0200

    homework3

---
+++ js/1script.js
@@ -0,0 +1,16 @@
+let x=prompt('Enter the first nubmer');
+let y=prompt('Enter the second number');
+
+alert (`//User entered ${x} and ${y} numbers:
+    ${x} + ${y} = ${x + y}
+
+    ${x} - ${y} = ${x - y}
+
+    ${x} * ${y} = ${x * y}
+
+    ${x} / ${y} = ${x / y}
+`)
+
+
+
+
